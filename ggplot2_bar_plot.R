ba7688e6d071b7b6340f425142582cd89520e6a7
gyanmishra <j12mishra@gmail.com>
2017-09-15 13:19:03 +0530

    Update ggplot2_bar_plot.R

--- ggplot2_bar_plot.R
+++ ggplot2_bar_plot.R
@@ -14,13 +14,6 @@ ggplot(dfm,aes(x = X1,y = value,fill=X2)) +
   #scale_y_continuous(expand = c(0,0),limits = c(0,200))+
   #scale_x_discrete(limit = c("Up", "Down"), labels = c("",""))+
   #geom_text(aes(label = value,group = value),colour="black", ,size = 5, hjust = 0.5, vjust = 1, position=position_stack(vjust = 0.5))+
-  theme(axis.text.x=element_text(size=15,face="",angle = 0)
-        ,axis.title.x=element_text(size=10)
-        ,axis.text.y=element_text(size=10 ,colour = "black",face="bold")
-        ,axis.title.y=element_text(size=18)
-        ,plot.title=element_text("Uns", size=15, face="bold", color="darkgreen")
-        ,legend.title = element_text(colour="black", size=10),) +
-  labs(x = "",y="No. of Genes",label =FALSE,title = "") +
   #scale_fill_discrete(name="Distance to TSS",breaks=c("Uns", "Promoter"),labels=c("< -1000\n>\t300", "-1000 to 300"))
   guides(fill=guide_legend(title="",label.theme = element_text(angle = 0,size = 10))) +
   #ylim(0,400)+
